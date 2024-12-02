ff156b7e3f77bab0b9048135fc64c49dd1402d16
Suraiya bilkis <kadermd246@gmail.com>
2024-12-03 00:41:12 +0600

    type casting

---
+++ typecasting.cs
@@ -0,0 +1,13 @@
+using System;
+
+class Program
+{
+    public static void Main(string[] agrs)
+    {
+        double salary= 2556.762;
+        Console.WriteLine(salary);
+        int salary2 =(int)salary;
+        //type casting (mane data type change kora)
+        Console.WriteLine(salary2);
+    }
+}

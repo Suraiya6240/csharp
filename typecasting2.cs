138c4ec8809840f53435533f853a662a8f66e847
Suraiya bilkis <kadermd246@gmail.com>
2024-12-03 15:58:07 +0600

    type casting 2

---
+++ typecasting2.cs
@@ -0,0 +1,14 @@
+using System;
+
+class Program
+{
+    public static void Main(string[] agrs)
+    {
+        double salary= 2556.54;
+        int salary2 =Convert.ToInt32(salary);
+        Console.WriteLine($"salary2={salary2}");
+       
+        Console.ReadKey();
+
+    }
+}

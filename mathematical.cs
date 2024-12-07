4168f0e3ea5b00c973677383fbf2d6d8b77e50a1
Suraiya bilkis <kadermd246@gmail.com>
2024-12-07 09:39:09 +0600

    Mathematical method

---
+++ mathematical.cs
@@ -0,0 +1,10 @@
+using System;
+class Test
+{
+    public static void Main(string[] agrs)
+    {
+        // int result = 25+30;
+        // console.Writeline(result);
+         Console.WriteLine(30+(24*2));
+    }
+}

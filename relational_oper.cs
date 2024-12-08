bf34c970cd437d90a673b4c70f2fcd26b28ce621
Suraiya bilkis <kadermd246@gmail.com>
2024-12-08 21:19:36 +0600

    realtional operator

---
+++ relational_oper.cs
@@ -0,0 +1,19 @@
+using System;
+
+class Test
+{
+    public static void Main(string[] args)
+    {
+        //relational operators
+        int number1 =20;
+        int number2 = 10;
+        int number3 = 50;
+       Console.WriteLine(number1 > number2 );
+       Console.WriteLine(number1 >= number2 );
+       Console.WriteLine(number1 < number2 );
+       Console.WriteLine(number1 <= number2 );
+       Console.WriteLine(number1 == number2 );
+       Console.WriteLine(number1 != number2 );
+      
+    }
+}

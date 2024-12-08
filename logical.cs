a8cda5f6cd07e5798a4d35df23300f191dddde77
Suraiya bilkis <kadermd246@gmail.com>
2024-12-08 21:30:07 +0600

    logical operator

---
+++ logical.cs
@@ -0,0 +1,13 @@
+using System;
+
+class Test
+{
+    public static void Main(string[] args)
+    {
+        //logical operators
+         Console.WriteLine(8>5 && 8<9);
+          Console.WriteLine(8>5 && 8<7);
+           Console.WriteLine(8<5 || 8<9);
+          Console.WriteLine(!(8>5 || 8<9)); //! not means that false mean true or true mean false
+    }
+}

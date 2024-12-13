c2bb803b3c526f763a5c7435ad5b90215dcc9aec
Suraiya bilkis <kadermd246@gmail.com>
2024-12-13 09:17:21 +0600

    Conditional statement

---
+++ conditional.cs
@@ -0,0 +1,27 @@
+using System;
+
+class Test
+{
+    public static void Main(string[] args)
+    {
+        //condition statement
+        int number =30;
+        if(number>0)
+        {
+    Console.WriteLine("Postive Number");
+        }
+       else if(number<0)
+        {
+    Console.WriteLine("Negative Number");
+        }
+        else  if(number==0)
+         {
+    Console.WriteLine("Zero Number");   
+            }
+          
+        
+        
+           
+
+    }
+}
